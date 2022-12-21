59c8c002026829ec94b49bce01f0e46684b2a79f
Nando <nandovga@hotmail.com>
2022-12-21 18:04:55 -0300

    #Tela de login

--- App.tsx
+++ App.tsx
@@ -1,20 +1,21 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import {StatusBar} from 'react-native';
+import {NativeBaseProvider} from "native-base"
+import {useFonts, Roboto_400Regular, Roboto_700Bold} from '@expo-google-fonts/roboto';
+
+import {THEME} from "./src/theme";
+import {Loading} from "@components/Loading";
+import {Signin} from "@screens/Signin";
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+    const [fontsLoaded] = useFonts({Roboto_400Regular, Roboto_700Bold});
+
+    return (
+        <NativeBaseProvider theme={THEME}>
+            <StatusBar barStyle="light-content"
+                       backgroundColor="transparent"
+                       translucent/>
+            {fontsLoaded ? <Signin /> : <Loading />}
+        </NativeBaseProvider>
+    );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
---
+++ src/components/Input.tsx
@@ -0,0 +1,22 @@
+import {Input as NativeBaseInput, IInputProps} from "native-base"
+
+export function Input({...rest}: IInputProps) {
+    return (
+        <NativeBaseInput
+            h={14}
+            px={4}
+            mb={4}
+            bg="gray.700"
+            borderWidth={0}
+            fontSize="md"
+            color="white"
+            fontFamily="body"
+            placeholderTextColor="gray.300"
+            _focus={{
+                bg: "gray.700",
+                borderWidth: 1,
+                borderColor: "green.500"
+            }}
+            {...rest}/>
+    );
+}
