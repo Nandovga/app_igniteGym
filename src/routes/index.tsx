9118d46e21af0f2e1ef34f8df41297e84454f0d2
Nando <nandovga@hotmail.com>
2022-12-22 14:45:22 -0300

    #Navegação privadas

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,6 +1,8 @@
 import {useTheme, Box} from "native-base";
 import {NavigationContainer, DefaultTheme} from "@react-navigation/native";
+
 import {AuthRoutes} from "@routes/auth.routes";
+import {AppRoutes} from "@routes/app.routes";
 
 export function Routes() {
 
@@ -11,7 +13,7 @@ export function Routes() {
     return (
         <Box flex={1} bg="gray.700">
             <NavigationContainer theme={theme}>
-                <AuthRoutes/>
+                <AppRoutes/>
             </NavigationContainer>
         </Box>
     );
